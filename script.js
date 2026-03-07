8084fd2cf6580ea0c28cd7fda6de261fae206a5a
HoopsUI <kanikabhatt15@gmail.com>
2026-03-07 18:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -814,3 +814,19 @@ if (resetCalculatorBtn) {
   });
 
 }
+
+// FAQ toggle
+
+document.querySelectorAll(".seo-section h3").forEach((question)=>{
+question.addEventListener("click",()=>{
+
+const answer = question.nextElementSibling;
+
+if(answer.style.display==="block"){
+answer.style.display="none";
+}else{
+answer.style.display="block";
+}
+
+});
+});
