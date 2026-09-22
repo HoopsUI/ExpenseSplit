a81ada9edb0e2137cafe000f31c53207c87a461a
HoopsUI <kanikabhatt15@gmail.com>
2026-09-22 20:05:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -845,3 +845,14 @@ window.addEventListener("load", function () {
 
 
 
+/*EZOIC PRIVACY SCRIPT*/
+<script data-cfasync="false" src="https://cmp.gatekeeperconsent.com/min.js"></script>
+<script data-cfasync="false" src="https://the.gatekeeperconsent.com/cmp.min.js"></script>
+
+/*EZOIC HEADER SCRIPT*/
+<script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+<script>
+    window.ezstandalone = window.ezstandalone || {};
+    ezstandalone.cmd = ezstandalone.cmd || [];
+</script>
+<script src="//ezoicanalytics.com/analytics.js"></script>
